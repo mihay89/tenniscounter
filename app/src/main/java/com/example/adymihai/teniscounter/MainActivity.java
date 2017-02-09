bf72e99f827e4b29550af1d9b2f63dfd4c81bdea
mihay89 <mihay.badea89@gmail.com>
2017-02-09 22:49:13 +0200

    I replaced a bunch of methods with a listener wich is pretty great for how the code looks. It s pretty great for me as, at this moment i don t even know what some of the methods are doing and where are coming from.

--- app/src/main/java/com/example/adymihai/teniscounter/MainActivity.java
+++ app/src/main/java/com/example/adymihai/teniscounter/MainActivity.java
@@ -5,12 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.app.Activity;
+
 import java.text.NumberFormat;
+
 import com.example.adymihai.teniscounter.R;
 
+import static android.R.id.button2;
+import static android.R.id.button3;
+
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout buttonPanel;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+
+
     int firstSetScoreFederer = 0;
     int secondSetScoreFederer = 0;
     int thirdSetScoreFederer = 0;
@@ -18,16 +35,63 @@ public class MainActivity extends AppCompatActivity {
     int secondSetScoreNadal = 0;
     int thirdSetScoreNadal = 0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForFedererOne(0);
-        displayForFedererTwo(0);
-        displayForFedererThree(0);
-        displayForNadalOne(0);
-        displayForNadalTwo(0);
-        displayForNadalThree(0);
+
+        buttonPanel = (LinearLayout) findViewById(R.id.buttonPanel);
+        button1 = (Button) findViewById(R.id.one_federer);
+        button2 = (Button) findViewById(R.id.one_nadal);
+        button3 = (Button) findViewById(R.id.two_federer);
+        button4 = (Button) findViewById(R.id.two_nadal);
+        button5 = (Button) findViewById(R.id.three_federer);
+        button6 = (Button) findViewById(R.id.three_nadal);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstSetScoreFederer = firstSetScoreFederer + 1;
+                displayForFedererOne(firstSetScoreFederer);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firstSetScoreNadal = firstSetScoreNadal + 1;
+                displayForNadalOne(firstSetScoreNadal);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondSetScoreFederer = secondSetScoreFederer + 1;
+                displayForFedererTwo(secondSetScoreFederer);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondSetScoreNadal = secondSetScoreNadal + 1;
+                displayForNadalTwo(secondSetScoreNadal);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thirdSetScoreFederer = thirdSetScoreFederer + 1;
+                displayForFedererThree(thirdSetScoreFederer);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thirdSetScoreNadal = thirdSetScoreNadal + 1;
+                displayForNadalThree(thirdSetScoreNadal);
+            }
+        });
 
     }
 
@@ -53,58 +117,32 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Increase the score for Team A by 1 point.
-     */
-    public void setOneFederer(View v) {
-        firstSetScoreFederer = firstSetScoreFederer + 1;
-            displayForFedererOne(firstSetScoreFederer);
-
-    }
-    public void setTwoFederer(View v) {
-        secondSetScoreFederer = secondSetScoreFederer + 1;
-            displayForFedererTwo(secondSetScoreFederer);
-
-    }
-    public void setThreeFederer(View v) {
-        thirdSetScoreFederer = thirdSetScoreFederer + 1;
-            displayForFedererThree(thirdSetScoreFederer);
-    }
-
-    public void setOneNadal(View v) {
-    firstSetScoreNadal = firstSetScoreNadal + 1;
-        displayForNadalOne(firstSetScoreNadal);
-
-}
-    public void setTwoNadal(View v) {
-    secondSetScoreNadal = secondSetScoreNadal + 1;
-        displayForNadalTwo(secondSetScoreNadal);
-}
-    public void setThreeNadal(View v) {
-        thirdSetScoreNadal = thirdSetScoreNadal + 1;
-            displayForNadalThree(thirdSetScoreNadal);
-        }
 
     public void displayForFedererOne(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_one_federer);
         scoreView.setText(String.valueOf(score));
     }
+
     public void displayForFedererTwo(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_two_federer);
         scoreView.setText(String.valueOf(score));
     }
+
     public void displayForFedererThree(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_three_federer);
         scoreView.setText(String.valueOf(score));
     }
+
     public void displayForNadalOne(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_one_nadal);
         scoreView.setText(String.valueOf(score));
     }
+
     public void displayForNadalTwo(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_two_nadal);
         scoreView.setText(String.valueOf(score));
     }
+
     public void displayForNadalThree(int score) {
         TextView scoreView = (TextView) findViewById(R.id.set_three_nadal);
         scoreView.setText(String.valueOf(score));
